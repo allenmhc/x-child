9c1c0777dd17cb07d1aa1328e198a53618f220ef
Allen Cheung <me@allenc.com>
2015-05-21 02:26:00 -0700

    Override entry meta formatting.

--- functions.php
+++ functions.php
@@ -127,3 +127,97 @@ if ( ! function_exists( 'x_get_content_layout' ) ) :
 
   }
 endif;
+
+// Entry Meta
+// =============================================================================
+
+if ( !function_exists( 'x_ethos_entry_meta' ) ) :
+  function x_ethos_entry_meta() {
+
+    $date = sprintf( '<span><time class="entry-date" datetime="%1$s">%2$s</time></span>',
+      esc_attr( get_the_date( 'c' ) ),
+      esc_html( get_the_date() )
+    );
+
+    if ( get_post_type() == 'x-portfolio' ) {
+      if ( has_term( '', 'portfolio-category', NULL ) ) {
+        $categories        = get_the_terms( get_the_ID(), 'portfolio-category' );
+        $separator         = ', ';
+        $categories_output = '';
+        foreach ( $categories as $category ) {
+          $categories_output .= '<a href="'
+                              . get_term_link( $category->slug, 'portfolio-category' )
+                              . '" title="'
+                              . esc_attr( sprintf( __( "View all posts in: &ldquo;%s&rdquo;", '__x__' ), $category->name ) )
+                              . '"> '
+                              . $category->name
+                              . '</a>'
+                              . $separator;
+        }
+
+        $categories_list = sprintf( '<span>%1$s %2$s',
+          __( 'Posted in', '__x__' ),
+          trim( $categories_output, $separator )
+        );
+      } else {
+        $categories_list = '';
+      }
+    } else {
+      $categories        = get_the_category();
+      $separator         = ', ';
+      $categories_output = '';
+      foreach ( $categories as $category ) {
+        $categories_output .= '<a href="'
+                            . get_category_link( $category->term_id )
+                            . '" title="'
+                            . esc_attr( sprintf( __( "View all posts in: &ldquo;%s&rdquo;", '__x__' ), $category->name ) )
+                            . '"> '
+                            . $category->name
+                            . '</a>'
+                            . $separator;
+      }
+
+      $categories_list = sprintf( '<span>%1$s %2$s</span>',
+        __( 'Posted in', '__x__' ),
+        trim( $categories_output, $separator )
+      );
+    }
+
+    if ( comments_open() ) {
+
+      $title  = apply_filters( 'x_entry_meta_comments_title', get_the_title() );
+      $link   = apply_filters( 'x_entry_meta_comments_link', get_comments_link() );
+      $number = apply_filters( 'x_entry_meta_comments_number', get_comments_number() );
+
+      if ( $number == 0 ) {
+        $text = __( 'Leave a Comment' , '__x__' );
+      } else if ( $number == 1 ) {
+        $text = $number . ' ' . __( 'Comment' , '__x__' );
+      } else {
+        $text = $number . ' ' . __( 'Comments' , '__x__' );
+      }
+
+      $comments = sprintf( '<span><a href="%1$s" title="%2$s" class="meta-comments">%3$s</a></span>',
+        esc_url( $link ),
+        esc_attr( sprintf( __( 'Leave a comment on: &ldquo;%s&rdquo;', '__x__' ), $title ) ),
+        $text
+      );
+
+    } else {
+
+      $comments = '';
+
+    }
+
+    if ( x_does_not_need_entry_meta() ) {
+      return;
+    } else {
+      printf( '<p class="p-meta">%1$s%2$s%3$s</p>',
+        $categories_list,
+        $date,
+        $comments
+      );
+    }
+
+  }
+endif;
