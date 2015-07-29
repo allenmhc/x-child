7fde49157908cc9602f7ea872fec3bc32532d04e
Allen Cheung <me@allenc.com>
2015-07-28 23:07:31 -0700

    Update comment for archive calendar JS

--- js/archive-calendar.js
+++ js/archive-calendar.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($) {
   var MAX_BULLET_COUNT = 10;
-
   var $calendar = $('.widget_archives_calendar .calendar-archives');
   $calendar.find('.postcount .count-number').each(function(idx, countNumber) {
-    // Replace a month's postcount numeric value with bullets, up to a max
+    // Replace a month's postcount numeric value with bullets, up to
+    // MAX_BULLET_COUNT
     var count = Math.min(parseInt(countNumber.textContent, 10), MAX_BULLET_COUNT);
     var bullets = $.map(new Array(count), function() { return "&#x25cf;"; });
     $(countNumber).html(bullets.join(' '));
